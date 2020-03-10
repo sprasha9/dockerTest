7bb5358f0b0011b4b17090f40299009f577656c6
sprasha9 <mail.prashanthsekar@gmail.com>
2020-03-10 17:44:53 +0530

    Add files via upload

---
+++ springboot-poc-cloud-config-server/src/main/java/com/springboot/poc/SpringbootPocCloudConfigServerApplication.java
@@ -0,0 +1,15 @@
+package com.springboot.poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class SpringbootPocCloudConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootPocCloudConfigServerApplication.class, args);
+	}
+
+}
